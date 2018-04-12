43a81d25de2ada596839c4494f4d5e56c0d56447
Daniel <daricaba@hotmail.com>
2018-04-12 05:21:56 +0200

    Ejemplos de uso de html helpers

---
+++ M0502/Controllers/ProgramaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M0502.Controllers
+{
+    public class ProgramaController : Controller
+    {
+        // GET: Programa
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Display(int id)
+        {
+            var Context = new Models.SaludMovilEntities();
+            var Programa = Context.sm_Programa.Find(id);
+            return View(Programa);
+        }
+
+        //public FileStreamResult GetImage()
+        //{
+
+        //}
+    }
+}
