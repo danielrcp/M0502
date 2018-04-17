882fb6dd51aa810481d20b445e64254fbe3ee267
Daniel <daricaba@hotmail.com>
2018-04-17 04:44:40 +0200

    Helpers de formulario, y manejo de archivos

--- M0502/Controllers/ProgramaController.cs
+++ M0502/Controllers/ProgramaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using M0502.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,7 +18,7 @@ namespace M0502.Controllers
         public ActionResult Display(int id)
         {
             var Context = new Models.SaludMovilEntities();
-            var Programa = Context.sm_Programa.Find(id);
+            var Programa = Context.sm_Programa.Where(p => p.idPrograma == id).FirstOrDefault();
             return View(Programa);
         }
 
@@ -25,5 +26,50 @@ namespace M0502.Controllers
         //{
 
         //}
+
+        public ActionResult Create()
+        {
+            var Programa = new sm_Programa();
+            return View(Programa);
+        }
+
+        [HttpPost]
+        public ActionResult Create(sm_Programa newPrograma)
+        {
+            ActionResult Result = View(newPrograma);
+            if (ModelState.IsValid)
+            {
+                using (var Context = new SaludMovilEntities())
+                {
+                    Context.sm_Programa.Add(newPrograma);
+                    Context.SaveChanges();
+                    Result = RedirectToAction("Display", new { id = newPrograma.idPrograma});
+                }
+            }
+            return Result;
+        }
+        [HttpPost]
+        public ActionResult UploadPhoto2(sm_Programa newPrograma, HttpPostedFileBase file)
+        {
+            ActionResult Result = View(newPrograma);
+
+            if (file != null && file.ContentLength > 0)
+            {
+                Result = File(file.InputStream, file.ContentType);
+            }
+            return Result;
+        }
+
+        [HttpPost]
+        public ActionResult UploadPhoto(sm_Programa newPrograma)
+        {
+            ActionResult Result = View(newPrograma);
+
+            if (Request.Files.Count > 0 && Request.Files[0].ContentLength> 0)
+            {
+                Result = File(Request.Files[0].InputStream, Request.Files[0].ContentType);
+            }
+            return Result;
+        }
     }
 }
---
+++ M0502/Models/ProgramaMetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace M0502.Models
+{
+    public partial class sm_Programa
+    {
+        [MetadataType(typeof(ProgramaMetaData))]
+        class ProgramaMetaData
+        {
+            [Display(Name ="Nombre del programa:")]
+            public object descripcion { get; set; }
+            [Display(Name = "Estado del programa:")]
+            public object idEstado { get; set; }
+        }
+    }
+}
